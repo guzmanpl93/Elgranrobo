e3f74f802ec048afd0561f9c3589fde177cb3ab0
andrea <mgd02452@educastur.es>
2025-03-10 15:27:36 +0100

    Andrea: Equipos y Bases

--- el gran robo/game/src/game/Bases.java
+++ el gran robo/game/src/game/Bases.java
@@ -1,5 +1,50 @@
 package game;
 
 public class Bases {
-
+	
+	//atributos
+	private int Id_base;
+	private int Id_equipo;
+	private int [] Ubicacion;
+	
+	//Constructores
+		public Bases(int Id_base, int Id_equipo, int [] Ubicacion) {
+			this.Id_base = Id_base;
+			this.Id_equipo = Id_equipo;
+			this.Ubicacion = Ubicacion;
+		}
+	
+	//Metodos 
+	public int[] lugarInicioBase() {
+		return Ubicacion != null ? Ubicacion : null;
+	}
+	
+	public void mostrarUbicacion() {
+		if (Ubicacion != null && Ubicacion.length == 2) {
+			System.out.println("La base " + Id_base + " del equipo " + Id_equipo + " se encuentra en la posicion: (" + Ubicacion[0] + ", " + Ubicacion[1] + ")");
+		} else {
+			System.out.println("La ubicacion de la base " + Id_base + " no esta definida correctamente.");
+		}
+	}
+	
+	//Getter y Setter
+	public int getId_Base() {
+		return Id_base;
+	}
+	public void setId_Base(int Id_base) {
+		this.Id_base = Id_base;
+	}
+	public int getId_equipo() {
+		return Id_equipo;
+	}
+	public void setId_equipo(int id_equipo) {
+		this.Id_equipo = id_equipo;
+	}
+	public int[] getUbicacion() {	
+		return Ubicacion;
+	}
+	public void setUbicacion(int[] Ubicacion) {
+		this.Ubicacion = Ubicacion;
+	}
+	
 }
--- el gran robo/game/src/game/Equipo.java
+++ el gran robo/game/src/game/Equipo.java
@@ -1,5 +1,55 @@
 package game;
 
-public class Equipo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Equipo {
+	
+	//atributos
+	private int id_equipo;
+	private String nombre;
+	private List<Jugadores> Jugador;
+	
+	//Constructores
+	public Equipo(int id_equipo, String nombre) {
+		this.id_equipo = id_equipo;
+		this.nombre = nombre;
+		this.Jugador = new ArrayList<>();
+	}
+
+	//	Metodos
+	public void agregarJugadores(Jugadores jugador) {
+//		Jugadores.add(Jugador);
+	}
+	
+	//Getters y Setters
+	public int getId_equipo() {
+		return id_equipo;
+	}
+
+
+	public void setId_equipo(int id_equipo) {
+		this.id_equipo = id_equipo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public List<Jugadores> getJugador() {
+		return Jugador;
+	}
+
+
+	public void setJugador(List<Jugadores> jugador) {
+		Jugador = jugador;
+	}
 }
+
